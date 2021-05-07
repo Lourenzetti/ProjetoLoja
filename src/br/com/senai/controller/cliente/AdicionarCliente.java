2b05a61b14fe441362396978dacfe4d2cba0471c
Lourenzetti <viniciuslourenzetti14@gmail.com>
2021-05-07 10:40:03 -0300

    Configuração dos métodos do cliente

---
+++ src/br/com/senai/controller/cliente/AdicionarCliente.java
@@ -0,0 +1,16 @@
+package br.com.senai.controller.cliente;
+
+import java.util.Scanner;
+
+public class AdicionarCliente {
+
+	Scanner entrada = new Scanner(System.in);
+	
+	public String definirCliente() {
+		
+		System.out.println("Informe o nome do Cliente: ");
+		String nome = entrada.nextLine();
+		return nome;
+	}
+	
+}
